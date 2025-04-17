8639de95022b6dde039dff30faaa9576caafbd04
singyuKang <rkdtlscks123@naver.com>
2025-04-17 23:20:42 +0900

    fix : 추가버튼 비활성화 제거

--- src/advanced/components/CartAddButton.tsx
+++ src/advanced/components/CartAddButton.tsx
@@ -2,19 +2,14 @@ import React from 'react';
 
 interface CartAddButtonProps {
   onClick: () => void;
-  disabled?: boolean;
 }
 
-const CartAddButton: React.FC<CartAddButtonProps> = ({
-  onClick,
-  disabled = false,
-}) => {
+const CartAddButton: React.FC<CartAddButtonProps> = ({ onClick }) => {
   return (
     <button
       id="add-to-cart"
       className="bg-blue-500 text-white px-4 py-2 rounded"
       onClick={onClick}
-      disabled={disabled}
     >
       추가
     </button>
